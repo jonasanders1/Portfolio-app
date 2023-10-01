8a14e33070a96eab12b53536921dc13cf64bd1f9
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-10-01 16:15:44 +0200

    initial commit

---
+++ portfolio/src/components/home/Social.tsx
@@ -0,0 +1,21 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+       
+        <a href="https://github.com/jonasanders1" className="home__social-icon" target='_blank'>
+            <i className="uil uil-github"></i>
+        </a>
+        
+        <a href="https://www.linkedin.com/in/jonas-andersen-65a335262/" className="home__social-icon" target='_blank'>
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+        <a href="#" className="home__social-icon" target='_blank'>
+            <i className="uil uil-instagram"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
